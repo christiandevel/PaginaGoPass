e5bce55d94ed0b832dfb97786cfd913cd649d6c7
JHON CHAVEZ <SEGUROSNOVUS\jhon.chavez@desarrollo-004.segurosnovus.com.co>
2020-10-30 18:03:43 -0500

    inicio pagina web, Gopass. Actualizacion

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+import '../node_modules/@fortawesome/fontawesome-free/js/all.js';
+console.log('Hola MIndo')
+console.log('Hola gordo')
+
